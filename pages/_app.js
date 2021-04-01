91a09095ca52dcefa16bc2437fd0d5d0db1fc0c7
wbrijesh <brijesh@wawdhane.com>
2021-04-01 10:45:50 +0530

    Hide unauthorized items from navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,41 @@
 import "../styles/globals.css";
 import "../configureAmplify";
 import Link from "next/link";
+import { Auth } from "aws-amplify";
 
 function MyApp({ Component, pageProps }) {
+  var seen = [];
+  let authdata = JSON.stringify(Auth, function (key, val) {
+    if (val != null && typeof val == "object") {
+      if (seen.indexOf(val) >= 0) {
+        return;
+      }
+      seen.push(val);
+    }
+    return val;
+  });
+  let stringifiedauthdata = JSON.stringify(authdata);
+
+  var status = new RegExp("student").test(stringifiedauthdata);
+
+  if (status) {
+    return (
+      <div>
+        <nav className="p-6 border-b border-gray-300">
+          <Link href="/">
+            <span className="mr-6 cursor-pointer">Home</span>
+          </Link>
+          <Link href="/profile">
+            <span className="mr-6 cursor-pointer">Profile</span>
+          </Link>
+        </nav>
+        <div className="py-8 px-16">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <nav className="p-6 border-b border-gray-300">
